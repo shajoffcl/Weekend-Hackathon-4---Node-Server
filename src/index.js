a88a3de2628a9b6cdd505052ef06b7f0ec383fa1
shajoffcl <shaj.native@gmail.com>
2020-11-07 18:17:16 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+    if(req.url==="/welcome"){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write("Hello");
+        res.end();
+    }
+    else if(req.url==="/contact"){
+        res.writeHead(200, {'Content-Type':'application/json'});
+        const contact={
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+        }
+        res.write(JSON.stringify(contact));
+        res.end();
+    }
+    else{
+        res.StatusCode=404;
+        res.end();
+    }
 }
 
+httpServer.listen(8081);
 module.exports = httpServer;
